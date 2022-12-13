49a1bc78728bc9f01b6b08464326614ac9bd0664
Ruslan Akbashev <ruslashev@gmail.com>
2022-12-14 03:46:06 +0400

    add benchmark mode

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,11 @@ fn main() {
 
     let mut state = State::new(window.as_inner());
 
+    if is_benchmark_mode() {
+        benchmark(window, state);
+        return;
+    }
+
     let updates_per_second: i16 = 60;
     let dt = 1.0 / f64::from(updates_per_second);
 
@@ -72,4 +77,56 @@ fn main() {
             window.set_title(title);
         }
     }
+}
+
+fn is_benchmark_mode() -> bool {
+    let mut args = std::env::args();
+
+    args.any(|arg| matches!(arg.as_str(), "--benchmark" | "-b"))
+}
+
+fn benchmark(window: Window, state: State) {
+    let frames = 5_000;
+
+    match get_time_rendering_n_frames(window, state, frames) {
+        Some(seconds) => println!("Rendered {} frames in {} seconds", frames, seconds),
+        None => println!("Error"),
+    }
+}
+
+fn get_time_rendering_n_frames(mut window: Window, mut state: State, n: usize) -> Option<f64> {
+    let start_time = Window::current_time();
+
+    let updates_per_second: i16 = 60;
+    let dt = 1.0 / f64::from(updates_per_second);
+
+    let mut current_frame = 0;
+    let mut current_time = Window::current_time();
+
+    while window.running {
+        current_frame += 1;
+
+        if current_frame > n {
+            break;
+        }
+
+        let real_time = Window::current_time();
+
+        while current_time < real_time {
+            current_time += dt;
+            state.update(dt, current_time);
+        }
+
+        for event in window.poll_events() {
+            if matches!(event, Event::WindowResize(_, _)) {
+                return None;
+            }
+        }
+
+        state.present();
+    }
+
+    let elapsed = Window::current_time() - start_time;
+
+    Some(elapsed)
 }
